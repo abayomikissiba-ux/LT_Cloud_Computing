63cba147d31b3a1f47f57cf31457ed988794193e
SelebSolomon <selebsolomon@gmail.com>
2026-03-02 05:40:06 +0100

    i added content in the script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-hello world
+alert("hello world");
+
+Just alerting the word that its fun learning with M4ace. I am so excited to learn about cloud computing and how it can help me in my future career. I am looking forward to learning more about the different cloud platforms and how to use them effectively.
